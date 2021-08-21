0743acc731d2d52cf856b422b636ac66d7292252
vaishnavijha <vaishnavi.jha.poorna@gmail.com>
2021-08-21 10:13:46 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [people, setPeople] = useState(data);
   return <main>
     <section className="container">
-      <h3>{people.length} birthdays today</h3>
+      <h3>{people} birthdays today</h3>
       <List people={people}/>
       <button onClick={()=>{people.length!==0?setPeople([]):document.querySelector("h3").innerHTML="List already cleared"}}>clear all</button>
     </section>
